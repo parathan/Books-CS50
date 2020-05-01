719a5d2dd56c05124109cca6d2f935dada04f67d
Parathan Vakeesan <parathan243@gmail.com>
2020-04-30 22:46:03 -0400

    Further implementing Login interface

--- BooksBackend.py
+++ BooksBackend.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def start():
     return render_template("Home.html")
 
+@app.route("/Login")
+def login():
+    return render_template("Login.html")
+
 @app.route("/Registration")
 def registration():
     return render_template("Registration.html")
